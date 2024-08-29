88ab47350bfb86b4dfb58faf1d2afa88c4f79928
Sami1234a <salman01705708434@gmail.com>
2024-08-29 19:29:24 +0600

    my image load updated

---
+++ my card/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './componets/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <>
+    <Card/>
+  </>
+}
+
+export default App
---
+++ my card/src/componets/Card.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import "./card.css"
+
+const Card = () => {
+    const [img, setImg] = useState("https://www.pickfu.com/blog/wp-content/uploads/2019/09/test3.jpeg")
+    const handleSelectChange = (e) =>{
+        setImg(e.target.value)
+    }
+  return (
+    <div>
+        <select onChange={(e)=>handleSelectChange(e)} name="" id="">
+            <option value="">--Select--</option>
+            <option value="https://www.pickfu.com/blog/wp-content/uploads/2019/09/test3.jpeg">Jea Sree</option>
+            <option value="https://www.mockupdaddy.com/wp-content/uploads/edd/2020/08/Premium-Cosmetic-Psd-Products-Mockup.png">Cosmetic</option>
+            <option value="https://images.wondershare.com/pixcut/assets/blog/product-images-for-ecommerce-sitse-3.jpg">Rudys</option>
+            <option value="https://ekit.co.uk/GalleryEntries/ecommerce_solutions_and_services/MedRes_Product-presentation-2.jpg?q=27012012153123">Robix Cube</option>
+            <option value="">Product</option>
+        </select>
+    <div className="card">
+        <div className="card-image">
+            <img src={img} alt="" />       
+        </div>
+
+        <div className="card_text">
+            <h2>Product</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi magnam voluptates laborum quidem maxime aliquid temporibus quam excepturi itaque vel?</p>
+
+            <input type="text" /> 
+            <button>Add</button>
+           <button> Remove</button>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Card
